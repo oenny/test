e1000a60ef856a6045deb49dbd1ceb53b906f73a
Oenny <oenny@parksound.nl>
2015-03-16 09:12:42 +0100

    add gh-pages deployment script

---
+++ deploy.ps1
@@ -0,0 +1,20 @@
+#$uncommitted=git status --porcelain
+#if ($uncommitted) {
+#  echo "Uncommitted changes"
+#} else {
+#  echo no
+#}
+
+# commit changes
+git add -A
+git commit -m "publish gh-pages" --allow-empty
+$commit_hash = git log -n 1 --format=%H HEAD
+
+# commit changes in dist to gh-pages
+git symbolic-ref HEAD refs/heads/gh-pages
+git --work-tree dist add -A
+git --work-tree dist commit --allow-empty --amend -m "publish commit '$commit_hash'"
+
+# checkout master
+git symbolic-ref HEAD refs/heads/master
+git reset --hard
