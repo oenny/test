8717d652570ad11b10a9bf777dbd0487a5534941
Oenny <oenny@parksound.nl>
2015-03-16 09:44:51 +0100

    publish gh-pages

--- deploy.ps1
+++ deploy.ps1
@@ -2,14 +2,15 @@ $uncommitted=git status --porcelain
 if ($uncommitted) {
   # commit changes
   git add -A
-  git commit -m "publish gh-pages branch"
+  git commit -m "publish gh-pages"
 }
-$commit_hash = git log -n 1 --format=%H HEAD
 
-# commit gh-pages
+$publish_commit = git log -n 1 --format=%H HEAD
+
+# checkout gh-pages + commit
 git symbolic-ref HEAD refs/heads/gh-pages
 git --work-tree dist add -A
-git --work-tree dist commit --allow-empty --amend -m "publish commit '$commit_hash'"
+git --work-tree dist commit --allow-empty --amend -m "publish commit '$publish_commit'"
 
 # checkout master
 git symbolic-ref HEAD refs/heads/master
